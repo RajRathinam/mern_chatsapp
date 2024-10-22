ad93fcc76668728dbacccdc67f3397ca4c857c71
RajRathinam <rajrathinam2005@gmail.com>
2024-10-22 08:10:02 -0700

    new commit

--- frontend/src/Pages/Chatpage.js
+++ frontend/src/Pages/Chatpage.js
@@ -11,7 +11,7 @@ const Chatpage = () => {
 
   return (
     <div style={{ width: "100%" }}>
-      {user && <SideDrawer />}
+      {user && <Box p="8px 10px"><SideDrawer/></Box>}
       <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
         {user && <MyChats fetchAgain={fetchAgain} />}
         {user && (
